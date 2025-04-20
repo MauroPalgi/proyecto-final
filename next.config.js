0eac011696c4c6d1b8ab2be6dcfd3211a0e33519
MauroPalgi <34563729+MauroPalgi@users.noreply.github.com>
2025-04-20 18:30:28 -0300

    New build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  distDir: "docs", // GitHub Pages sirve desde /docs
   basePath: "/proyectofinal",
   images: {
     unoptimized: true, // porque no hay server
